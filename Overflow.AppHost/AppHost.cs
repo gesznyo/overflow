6e181f9080b6c01cff4a0f83d13c57c9de5a7bd7
gesznyo <janos.koczka@gmail.com>
2026-06-09 14:23:03 +0200

    End of section 3

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,7 +1,26 @@
+#pragma warning disable ASPIRECERTIFICATES001
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder
     .AddKeycloak(name:"keycloak", port: 6001)
-    .WithDataVolume("keycloak-data");
+    .WithDataVolume("keycloak-data")
+    .WithOtlpExporter()
+    .WithoutHttpsCertificate();
+
+var postgres = builder
+    .AddPostgres(name: "postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithOtlpExporter()
+    .WithoutHttpsCertificate()
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+var questionService = builder
+    .AddProject<Projects.QuestionService>(name: "question-svc")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
 
 builder.Build().Run();
