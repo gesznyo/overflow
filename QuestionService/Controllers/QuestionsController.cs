9744993567523202442d4a47a6d97cb48df64a7f
gesznyo <janos.koczka@gmail.com>
2026-06-18 08:28:42 +0200

    End of section 5

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -66,13 +66,15 @@ public class QuestionsController(QuestionDbContext db, IMessageBus bus, TagServi
     [HttpGet("{id}")]
     public async Task<ActionResult<Question>> GetQuestion(string id)
     {
-        var question = await db.Questions.FindAsync(id);
+        var question = await db.Questions
+            .Include(x => x.Answers)
+            .FirstOrDefaultAsync(x => x.Id == id);
         
         if (question is null) return NotFound();
         
-        await db.Questions.Where(q=>q.Id == id)
-            .ExecuteUpdateAsync(setters => setters
-                .SetProperty(q=> q.ViewCount, q=>q.ViewCount+1));
+        await db.Questions.Where(x => x.Id == id)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.ViewCount,
+                x => x.ViewCount + 1));
         
         return question;
     }
@@ -124,5 +126,90 @@ public class QuestionsController(QuestionDbContext db, IMessageBus bus, TagServi
         return NoContent();
     }
     
+    [Authorize]
+    [HttpPost("{questionId}/answers")]
+    public async Task<ActionResult> PostAnswer(string questionId, CreateAnswerDto dto)
+    {
+        var question = await db.Questions.FindAsync(questionId);
+        if (question is null) return NotFound();
+        
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var name = User.FindFirstValue("name");
+        if (userId is null || name is null) return BadRequest("Cannot get user details");
+        
+        var answer = new Answer
+        {
+            Content = dto.Content,
+            UserId = userId,
+            UserDisplayName = name,
+            QuestionId = questionId
+        };
+        
+        question.Answers.Add(answer);
+        question.AnswerCount++;
+        await db.SaveChangesAsync();
+        
+        await bus.PublishAsync(new AnswerCountUpdated(questionId, question.AnswerCount));
+        
+        return Created($"/questions/{questionId}", answer);
+    }
+    
+    [Authorize]
+    [HttpPut("{questionId}/answers/{answerId}")]
+    public async Task<ActionResult> UpdateAnswer(string questionId, string answerId, CreateAnswerDto dto)
+    {
+        var answer = await db.Answers.FindAsync(answerId);
+        if (answer is null) return NotFound();
+        
+        if (answer.QuestionId != questionId) 
+            return BadRequest("Cannot update answer details");
+        
+        answer.Content = dto.Content;
+        answer.UpdatedAt = DateTime.UtcNow;
+        await db.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
+
+    [Authorize]
+    [HttpDelete("{questionId}/answers/{answerId}")]
+    public async Task<ActionResult> DeleteAnswer(string questionId, string answerId)
+    {
+        var answer = await db.Answers.FindAsync(answerId);
+        var question = await db.Questions.FindAsync(questionId);
+        if (answer is null || question is null) return NotFound();
+        if (answer.QuestionId != questionId || answer.Accepted) return BadRequest("Cannot delete this answer");
+        
+        db.Answers.Remove(answer);
+        question.AnswerCount--;
+        await db.SaveChangesAsync();
+        
+        await bus.PublishAsync(new AnswerCountUpdated(questionId, question.AnswerCount));
+        
+        return NoContent();
+    }
+    
+    [Authorize]
+    [HttpPost("{questionId}/answers/{answerId}/accept")]
+    public async Task<ActionResult> AcceptAnswer(string questionId, string answerId)
+    {
+        var answer = await db.Answers.FindAsync(answerId);
+        var question = await db.Questions.FindAsync(questionId);
+        if (answer is null || question is null) return NotFound();
+        
+        if (answer.QuestionId != questionId || question.HasAcceptedAnswer) 
+            return BadRequest("Cannot accept answer");
+        
+        answer.Accepted = true;
+        question.HasAcceptedAnswer = true;
+        await db.SaveChangesAsync();
+        
+        await bus.PublishAsync(new AnswerAccepted(questionId));
+        
+        return NoContent();
+    }
+    
+
     
 }
